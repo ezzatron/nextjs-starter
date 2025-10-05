360abab1d41ae13ad1f004aa0f2718595fb78b34
Erin Millard-Wright <contact@erin.id.au>
2025-10-05 19:38:05 +1000

    Remove cruft

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,3 @@
-import Image from "next/image";
-import bananas from "./bananas.jpg";
-import styles from "./page.module.css";
-
 export default function Home() {
-  return (
-    <main className={styles.example}>
-      It works
-      <Image src={bananas} alt="Bunches of bananas" />
-    </main>
-  );
+  return <main>It works</main>;
 }
