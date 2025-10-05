72f6c971b24208e091e6fe16e7440d977fbd8e58
Erin Millard-Wright <contact@erin.id.au>
2025-10-05 22:04:35 +1000

    Replace Biome with ESLint and Prettier

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+// @ts-check
+import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+  recommendedConfig: js.configs.recommended,
+});
+
+/** @type {import("eslint").Linter.Config[]} */
+const config = [
+  { ignores: [".next", "next-env.d.ts"] },
+  ...compat.config({
+    extends: [
+      "eslint:recommended",
+      "next/core-web-vitals",
+      "next/typescript",
+      "prettier",
+    ],
+  }),
+];
+
+export default config;
--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const config: NextConfig = {};
 
-export default nextConfig;
+export default config;
