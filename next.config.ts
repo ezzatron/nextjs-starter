cde4274428c481341ce85409a271d16778aa6e03
Erin Millard-Wright <contact@erin.id.au>
2025-10-28 22:14:45 +1000

    Disable standalone mode when running "next start"

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
+// Detect if running "next start" and automatically disable standalone mode
+const isNextStart = process?.argv?.includes("start");
+
 const config: NextConfig = {
   distDir: "out/next/dist",
-  output: "standalone",
+  output: isNextStart ? undefined : "standalone",
 };
 
 export default config;
